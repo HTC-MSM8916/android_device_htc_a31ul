0278fe3654a84f341dca182333ed066d5c8616b9
Patrik Zakhar <123patokt@gmail.com>
2015-11-01 11:22:08 +0100

    a31ul: update wifi config files

--- device.mk
+++ device.mk
@@ -94,7 +94,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_engine.conf:system/etc/thermal-engine.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
