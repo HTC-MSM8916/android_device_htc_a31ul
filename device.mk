bc2d90e727640ee28e843136154722b1ae660c8b
Patrik Zakhar <123patokt@gmail.com>
2015-11-01 15:31:50 +0100

    a31ul: don't build camera HAL

--- device.mk
+++ device.mk
@@ -49,11 +49,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
-# Camera
-PRODUCT_PACKAGES += \
-    camera.msm8916 \
-    libmm-qcamera
-
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
